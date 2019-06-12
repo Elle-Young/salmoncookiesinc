79e6e88aef4ed4e4abef4a250f7e71ae583cc678
elle-young <missanyayoung@outlook.com>
2019-06-12 07:45:07 -0700

    update with random num gen added

--- js/index.js
+++ js/index.js
@@ -1,48 +1,45 @@
 'use strict';
 
-// var turing = {
-//     temp: 68,
-//     light: 9, 
-//     dim: function(num){
-//         this.light = num;
-//         this.heat(57);
-//     },
-// heat: function(num){
-//     this.temp = num;
-//     }
-
-// }
 var pike ={
   min: 23,
   max: 65,
   avgS: 6.3,
+  randomNumber: numGen(){
+    Math.floor((Math.random() * 10)+1);}
 }
-getNum: function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; 
-}
-console.log(getNum)
-
 
 var seaTac ={
-    min: 3, 
-    max: 24,
-    avgS: 1.2,
-}
+  min: 3,
+  max: 24,
+  avgS: 1.2,
+  randomNumber: function(){
+    Math.floor((Math.random() * 10)+1);}
+};
 
 var seaCntr ={
-    min: 11,
-    max: 38,
-    avgS: 3.7,
-}
+  min: 11,
+  max: 38,
+  avgS: 3.7,
+  randomNumber: function(){
+    Math.floor((Math.random() * 10)+1);}
+};
 var capH ={
-    min: 20,
-    max: 38,
-    avgS: 2.3,
-}
+  min: 20,
+  max: 38,
+  avgS: 2.3,
+};
 var alki ={
-    min: 2,
-    max: 16,
-    avgS: 4.6,
+  min: 2,
+  max: 16,
+  avgS: 4.6,
+  randomNumber: function(){
+    Math.floor((Math.random() * 10)+1);}
+};
+
+render: numGen(){
+  var divElement = document.getElementById('pike');
+  var newTitle = document.createElement(h2);
+  newTitle.textContent = this.name;
+  divElement.appendChild(newTitle);
 }
+console.log()
