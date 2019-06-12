88f23ca046fa26b027b66585ab13162a681dd95f
elle-young <missanyayoung@outlook.com>
2019-06-12 08:31:45 -0700

    First commit on Tuesday Branch, unfinished

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var pike ={
-  min: 23,
-  max: 65,
-  avgS: 6.3,
-  randomNumber: numGen(){
-    Math.floor((Math.random() * 10)+1);}
-}
+var store = new Object();
+store.name = 'Pike';
+store.min = 23;
+store.max = 65;
+store.avgS = 6.3;
 
-var seaTac ={
-  min: 3,
-  max: 24,
-  avgS: 1.2,
+checkSales = function randomNumber() {
+  Math.floor((Math.randomNumber() * 10)+1);
+
+
+
+new Object ={
+  store.min = 3,
+  store.max = 24,
+  store.avgS = 1.2,
   randomNumber: function(){
     Math.floor((Math.random() * 10)+1);}
 };
@@ -33,13 +36,15 @@ var alki ={
   max: 16,
   avgS: 4.6,
   randomNumber: function(){
-    Math.floor((Math.random() * 10)+1);}
+    Math.floor((Math.random() * 10)+1);
+  }
 };
 
-render: numGen(){
-  var divElement = document.getElementById('pike');
-  var newTitle = document.createElement(h2);
-  newTitle.textContent = this.name;
-  divElement.appendChild(newTitle);
-}
-console.log()
+numGen();
+var divElement = document.getElementById('pike');
+var newTitle = document.createElement(h2);
+newTitle.textContent = this.name;
+divElement.appendChild(newTitle);
+
+
+console.log(pike);
