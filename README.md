a882d2fe46c3e7783949f458cf123802c649f688
elle-young <missanyayoung@outlook.com>
2019-06-13 08:40:49 -0700

    update to constructors and functions in js, update to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # salmoncookiesinc
 Lab: Salmon Cookies
+This lab creates a webpage for Pat's Salmon Cookies and displays sales information about the business.
+
+Special thanks to https://github.com/strangestbrew (Lindsey) for help with troubleshooting, code examples, and overall being a great support! 
+
+**Please note the author of this site does not condone eating salmon cookies.**
--- js/index.js
+++ js/index.js
@@ -1,50 +1,42 @@
 'use strict';
 
-var store = new Object();
-store.name = 'Pike';
-store.min = 23;
-store.max = 65;
-store.avgS = 6.3;
+Var times = [ '6am', '7am', '8am', '9am', '10am', '11am', 'Noon', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-checkSales = function randomNumber() {
-  Math.floor((Math.randomNumber() * 10)+1);
-
-
-
-new Object ={
-  store.min = 3,
-  store.max = 24,
-  store.avgS = 1.2,
-  randomNumber: function(){
-    Math.floor((Math.random() * 10)+1);}
-};
-
-var seaCntr ={
-  min: 11,
-  max: 38,
-  avgS: 3.7,
-  randomNumber: function(){
-    Math.floor((Math.random() * 10)+1);}
-};
-var capH ={
-  min: 20,
-  max: 38,
-  avgS: 2.3,
-};
-var alki ={
-  min: 2,
-  max: 16,
-  avgS: 4.6,
-  randomNumber: function(){
-    Math.floor((Math.random() * 10)+1);
-  }
-};
-
-numGen();
-var divElement = document.getElementById('pike');
-var newTitle = document.createElement(h2);
-newTitle.textContent = this.name;
-divElement.appendChild(newTitle);
+function Store(name, minC, maxC, avgS){
+  this.name = name;
+  this.minC = minC;
+  this.maxC = maxC;
+  this.avgS = avgS;
+  this.salesArray = [];
+  Store.list.push(this);
+}
 
 
-console.log(pike);
+new Store('Pike Place', 23, 65, 6.5)
+new Store('Seatac', 3, 24, 1.2)
+new Store('SeaCentr', 11, 38, 3.7)
+new Store('Cap Hill', 20, 38, 2.3)
+new Store('Alki', 2, 16, 4.6)
+
+
+// var seaCntr ={
+//   min: 11,
+//   max: 38,
+//   avgS: 3.7,
+//   randomNumber: function(){
+//     Math.floor((Math.random() * 10)+1);}
+// };
+// var capH ={
+//   min: 20,
+//   max: 38,
+//   avgS: 2.3,
+// };
+// var alki ={
+//   min: 2,
+//   max: 16,
+//   avgS: 4.6,
+//   randomNumber: function(){
+//     Math.floor((Math.random() * 10)+1);
+//   }
+// };
+
