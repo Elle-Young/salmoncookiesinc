f12bdcfffe5bc04bd25c31c16e957130a59ef605
elle-young <missanyayoung@outlook.com>
2019-07-02 23:44:07 -0700

    new table update, still buggy

--- js/index.js
+++ js/index.js
@@ -1,49 +1,35 @@
 'use strict';
 
 
-function Place(name, minCustomer, maxCustomer){
+function Storeinfo(name, minCustomer, maxCustomer){
   this.name = name;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
-  Place.list.push(this);
+  Storeinfo.list.push(this);
 }
 
-Place.list =[];
+Storeinfo.list =[];
 
-Place.prototype.render = function(){
-  var tableBody = document.getElementById('stores');
-  var tableRow = document.getElementById('tr');
-  var cell = document.getElementById('td');
-  cell.textContent = this.name;
-  tableRow.appendChild(cell);
-  cell = document.createElement('td');cell.textContent = this.minCustomer;
-  tableRow.appendChild(cell);
-  cell = document.createElement('td');
-  cell.textContent = this.maxCustomer;
-  tableRow.appendChild(cell);
-  tableBody.appendChild(tableRow);
-};
 
-new Place('Pike', 23, 65, 6.5);
-new Place('Pike', 23, 65, 6.5);
-new Place('Pike', 23, 65, 6.5);
-//   function table(){
-//   };
- 
-// function(storeInfo){
-//     var rows = ('<tr></tr>');
-//     rows.append(('<td></td>').text(storeInfo.name));
-//     rows.push({
-//       storeInfo:storeInfo,
+new Storeinfo('Pike', 43, 65, 6.5); //0
+// new Place('PINE', 23, 99, 8.5); //1
+// new Place('Alki', 13, 65, 6.0); //2
+console.log(Storeinfo.list[0]);
+function table(){
+}
 
-//     });
-//   };
+function makeRows(){
+  var rows = ('<tr></tr>');
+  rows.append(('<td></td>').text Storeinfo.name));
+  rows.push({
+    storeInfo:Storeinfo,
 
-// table();
-// makeRows();
-// console.log(table());
-// console.log(makeRows());
-// console.log(Place())
-// }
+  });
+}
 
-// storeInfo();
+table();
+makeRows();
+console.log(table());
+console.log(makeRows());
+// console.log(Place());
+console.log(Storeinfo.list[0]);
